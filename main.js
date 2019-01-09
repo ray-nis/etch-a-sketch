d531a344ea2cb85d64c1ceede8bc425f397fa63b
ray-nis <reirei256@gmail.com>
2019-01-09 10:50:32 +0100

    stuff

--- main.js
+++ main.js
@@ -6,13 +6,52 @@ for (let i = 0; i < 32; i++) {
     container.appendChild(document.createElement('br'));
 }
 
+let color = "#ea6556";
+
 let elements = container.children;
 for (let i = 0; i < elements.length; i++) {
     if (elements[i].tagName == "DIV") {
         elements[i].classList.add("divs");
         elements[i].style.width = 900 / 64 + "px";
         elements[i].style.height = 900 / 64 + "px";
+        elements[i].addEventListener("mouseover", (e) => {
+            e.target.style.background = color;});
+        elements[i].addEventListener("click", (e) => {
+            e.target.style.background = "#fff";});
     }
 }
 
-//let divs = querySelectorAll(".divs");
+
+document.querySelector("#white").addEventListener("click", () => {
+    color = "#fff";
+});
+
+
+
+let colorOptions = document.querySelectorAll(".colorOption");
+
+colorOptions.forEach((item) => {
+    item.addEventListener('click', (e) => {
+        let id = e.target.id;
+        switch(id) {
+            case "blue":
+                color = "#8091d6";
+                break;
+            case "red":
+                color = "#ea6556";
+                break;
+            case "white":
+                color = "#fff";
+                break;
+            case "black":
+                color = "#000000";
+                break;
+            case "green":
+                color = "#85f27d";
+                break;
+            case "yellow":
+                color = "#f4d873";
+                break;
+        }
+    });
+});
